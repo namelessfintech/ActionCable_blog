ddbcb814684f52635764fd96dd5f509a3484dd75
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-06-18 12:01:45 -0400

    Handle the article not found error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  # how to trigger my exception catchers
+rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+
+  protected
+
+  def resource_not_found
+    # this will be overidden in my individual controllers
+  end
 end
