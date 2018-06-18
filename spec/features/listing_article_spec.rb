9d2ab78cd7535686be62d9f47e32712ec2fa76a7
Michael Anthony Ballard <MichaelBallard@Michaels-MBP.fios-router.home>
2018-06-18 00:05:30 -0400

    added the listings feature

---
+++ spec/features/listing_article_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+Rspec.feature "Listing Articles" do
+  before do
+    @article1 = Article.create(title: "The first article", body: "Lorem ipsum 1")
+
+    @article2 = Article.create(title: "The Second article", body: "Lorem ipsum 2")
+  end
+
+  scenario "A users lists all articles" do
+    visit '/'
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+
+  end
+
+end
