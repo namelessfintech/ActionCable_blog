3f89857053f6bea4dfd6c89e5e59987f113f0c88
Michael Anthony Ballard <MichaelBallard@Michaels-MBP.fios-router.home>
2018-06-18 20:56:31 -0400

    added user-signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
   protected
 
   def resource_not_found
-    # this will be overidden in my individual controllers
+    # this will be overidden in our individual controllers
   end
 end
