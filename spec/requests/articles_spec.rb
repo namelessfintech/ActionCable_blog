6b3d459b7acde0404163a0bd82bf156a7f709f63
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-06-19 13:24:03 -0400

    updated association restrictions

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -3,8 +3,49 @@ require 'rails_helper'
 RSpec.describe 'Articles', type: :request do
 
   before do
-    @article = Article.create(title: "Title One", body: "Body of article 1" )
+    @pablo = User.create(email:"pablo@example.com", password:"password")
+    @michael = User.create(email:"michael@example.com", password:"password")
+    @article = Article.create!(title: "Title One", body: "Body of article 1", user:@pablo )
   end
+
+  describe "GET/articles/:id/edit" do
+    context "with non-signed in user" do
+        before {get "/articles/#{@article.id}/edit"}
+
+        it "redirects to the signin page" do
+          expect(response.status).to eq 302
+          flash_message = "You need to sign in or sign up before continuing."
+          expect(flash[:alert]).to eq flash_message
+        end
+    end
+
+    context "with signed in user who is non-owner" do
+      before do
+        login_as(@michael)
+        get "/articles/#{@article.id}/edit"
+      end
+
+      it "redirects to the home page" do
+        expect(response.status).to eq 302
+        flash_message = "You can only edit your own article"
+        expect(flash[:alert]).to eq flash_message
+      end
+  end
+
+  context 'with signed in user as owner successful edit' do
+    before do
+      login_as(@pablo)
+      get "/articles/#{@article.id}/edit"
+    end
+
+    it "successfully edits article" do
+      expect(response.status).to eq 200
+    end
+
+  end
+
+
+end
 
   describe "GET /articles/:id " do
     context 'with existing article' do
