e1d00aa84ab25fea822b87b79cdfe9be31bd33ad
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-06-20 14:17:49 -0400

    Added new show and create for user profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,10 +5,30 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile
   end
 
+  def create
+    @user = find_user
+    @profile = @user.build_profile(set_params)
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_profile_path( params[:user_id] )
+    else
+      render action: :new
+    end
+  end
+
+  def show
+    @user = find_user
+  end
+
+
   private
 
   def find_user
       User.find(params[:user_id])
+  end
+
+  def set_params
+    params.require(:profile).permit(:first_name,:last_name,:bio)
   end
 
 end
