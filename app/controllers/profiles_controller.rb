8490870ef52c28423f0c4bcca9532af44aa17fa5
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-06-20 13:35:16 -0400

    Added new profiles action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,14 @@
 class ProfilesController < ApplicationController
+
+  def new
+    @user = find_user
+    @profile = @user.build_profile
+  end
+
+  private
+
+  def find_user
+      User.find(params[:user_id])
+  end
+
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,4 @@
 class Profile < ApplicationRecord
-  belong_to :user
+  belongs_to :user
 
 end
