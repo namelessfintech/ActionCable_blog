748a3c23d209eafd9862d33bff40df5cf253a57a
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-06-20 15:49:05 -0400

    Added before filters

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
 
   def new
     @user = find_user
@@ -23,11 +25,26 @@ class ProfilesController < ApplicationController
   def edit
     @user = find_user
     @profile = @user.profile
+  end
 
+  def update
+    @user = find_user
+    @profile = @user.profile
+    if @profile.update(set_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_profile_path( params[:user_id] )
+    else
+      render action: :edit
+    end
   end
 
 
   private
+
+  def only_current_user
+    @user = find_user
+    redirect_to(root_path) unless @user == current_user
+  end
 
   def find_user
       User.find(params[:user_id])
