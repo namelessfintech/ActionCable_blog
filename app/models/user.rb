53a3c6744c3d4d0c0c0ea6c77d55587d99f67963
prmoreira23 <prm.gredes@gmail.com>
2018-06-21 11:42:50 -0400

    Add friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,31 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
+  # Friends Actively
+  has_many :friendships_active, class_name:  "Friendship", foreign_key: "user_id", dependent: :destroy
+  has_many :friends, through: :friendships_active
+
+  # Friends passively
+  has_many :friendships_passive, class_name:  "Friendship", foreign_key: "friend_id", dependent: :destroy
+  has_many :users, through: :friendships_passive, class_name: "User"
+
+  # All friends
+  def all_friends
+    users + friends
+  end
+
+  # Friends a user
+  def friend(other_user)
+    friends << other_user
+  end
+
+  # Unriends a user.
+  def unfriend(other_user)
+    friends.delete(other_user)
+  end
+
+  # Returns true if the current user is friends with the other user.
+  def friends?(other_user)
+    all_friends.include?(other_user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2018_06_20_201744) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id"
     t.string "first_name"
