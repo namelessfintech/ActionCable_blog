7358be648e4a046bb7d5e1a2f4b4f3c4915b2451
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-06-21 16:53:10 -0400

    create chatroom when user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,25 @@ class User < ApplicationRecord
   # before_save {self.email = email.downcase}
   has_many :articles
   has_one :profile
+  has_one :room
   # validates :name, length:{maximum:20}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,length:{maximum: 64},
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  after_create :create_chatroom
+
+
+  def full_name
+    "#{self.profile.first_name}" + " " + "#{self.profile.last_name}"
+  end
+
+  private
+
+  def create_chatroom
+    hyphenated_username = self.full_name.split.join("-")
+    Room.create(name: hyphenated_username, user_id: self.id)
+  end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_201744) do
+ActiveRecord::Schema.define(version: 2018_06_21_204155) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 2018_06_20_201744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
